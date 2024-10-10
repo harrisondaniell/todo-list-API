8ee6f6d1efaabb8c26ab8f97cbf27c0fe02fb45f
harrison <d.harrison.lima@gmail.com>
2024-10-10 19:51:54 -0300

    feat: add repository getusers, update and delete

--- src/repositorys/user.repository.js
+++ src/repositorys/user.repository.js
@@ -15,3 +15,44 @@ export const createUserRepositorry = async (data) => {
 
   return user;
 };
+
+export const getAllUsersRepository = async () => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  return users;
+};
+
+export const updateUserRepository = async (id, data) => {
+  const user = await prisma.user.update({
+    where: {
+      id,
+    },
+    data,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  return user;
+};
+
+export const deleteUserRepository = async (id) => {
+  const user = await prisma.user.delete({
+    where: {
+      id,
+    },
+  });
+  return user;
+};
