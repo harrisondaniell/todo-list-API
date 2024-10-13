e84633191eec5426247aa7fb0bbb1a5f1071321d
harrison <d.harrison.lima@gmail.com>
2024-10-13 00:03:32 -0300

    fix: add router in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import express, { urlencoded } from "express";
+import express from "express";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 app.use(express.json());
-app.use(urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(userRouter);
 
 export default app;
