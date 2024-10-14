e1e2f516d1696d2196bdb0282fed340add968370
harrison <d.harrison.lima@gmail.com>
2024-10-14 19:22:58 -0300

    feat: add auth route

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,24 +3,19 @@ import "dotenv/config";
 
 export const Auth = {
   private: async (req, res, next) => {
-    let sucess = false;
-
     if (req.headers.authorization) {
       const [authType, token] = req.headers.authorization.split(" ");
       if (authType === "Bearer") {
         try {
           const decoded = JWT.verify(token, process.env.JWT_SECRET_KEY);
-
-          sucess = true;
-        } catch (error) {}
+          req.user = decoded;
+          console.log(req.user);
+          return next();
+        } catch (error) {
+          console.error("Erro na verificação do token:", error.message);
+        }
       }
     }
-
-    if (sucess) {
-      next();
-    } else {
-      res.status(403);
-      res.json({ error: "Não autorizado" });
-    }
+    res.status(403).json({ error: "Não autorizado" });
   },
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,14 +6,13 @@ import {
   updateUserController,
   deleteUserController,
 } from "../controllers/user.controller.js";
-// import { Auth } from "../middlewares/auth.js";
+import { Auth } from "../middlewares/auth.js";
 
 const userRouter = Router();
 
 userRouter.post("/user", createUserController);
-// userRouter.get("/users", Auth.private, getAllUsersController);
-userRouter.get("/users", getAllUsersController);
-userRouter.patch("/user/:id", updateUserController);
-userRouter.delete("/user/:id", deleteUserController);
+userRouter.get("/users", Auth.private, getAllUsersController);
+userRouter.patch("/user/:id", Auth.private, updateUserController);
+userRouter.delete("/user/:id", Auth.private, deleteUserController);
 
 export default userRouter;
