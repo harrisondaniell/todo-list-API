77094b635f2f023c0c5f32ee527178fefa8a4ae5
harrison <d.harrison.lima@gmail.com>
2024-10-14 19:49:26 -0300

    fix: token generate

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,20 +7,19 @@ import {
 export const login = async (req, res) => {
   try {
     if (req.body.email && req.body.password) {
-      let email = req.body.email;
-      let password = req.body.password;
+      const { email, password } = req.body;
 
-      let user = await findUserByEmail(email);
+      const user = await findUserByEmail(email);
       if (!user) {
         throw new Error("Usuário não cadastrado");
       }
 
       const isPasswordValid = await validatePassword(user, password);
       if (!isPasswordValid) {
-        throw new Error("Erro ao válidar credenciais");
+        throw new Error("Erro ao validar credenciais");
       }
 
-      const token = tokenGenerate(user.id, user.email);
+      const token = await tokenGenerate(user.id, user.email);
 
       res.json({ status: true, token });
     }
