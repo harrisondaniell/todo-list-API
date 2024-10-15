e5762cce1e3afd6c6129531ea29bcf958c6e9235
harrison <d.harrison.lima@gmail.com>
2024-10-14 21:22:23 -0300

    fix: cors config

--- src/app.js
+++ src/app.js
@@ -5,13 +5,6 @@ import taskRouter from "./routes/task.route.js";
 import authRouter from "./routes/auth.route.js";
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(authRouter);
-app.use(userRouter);
-app.use(taskRouter);
-
-app.use(cors());
 
 const corsOptions = {
   origin: "*",
@@ -21,7 +14,11 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors(corsOptions));
+app.use(authRouter);
+app.use(userRouter);
+app.use(taskRouter);
 
 export default app;
